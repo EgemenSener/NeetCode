19e83b14dd742ffb3cb731ac2de43d52cceebc42
Egemen Sener <egemensener98@gmail.com>
2024-03-03 16:15:28 +0300

    .

--- src/main/java/LinkedListCycle.java
+++ src/main/java/LinkedListCycle.java
@@ -6,9 +6,8 @@ public class LinkedListCycle {
     	HashSet<ListNode> visitedNodes = new HashSet<ListNode>();
     	ListNode currentNode = head;
     	while(currentNode != null) {
-    		if(visitedNodes.contains(currentNode)) {
+    		if(visitedNodes.contains(currentNode)) 
     			return true;
-    		}
     		visitedNodes.add(currentNode);
     		currentNode = currentNode.next;
     	}
@@ -19,15 +18,11 @@ public class LinkedListCycle {
 		
 		LinkedListCycle linkedListCycle = new LinkedListCycle();
 		
-		ListNode listNode1 = null;
-		
-		ListNode listNode3 = new ListNode(4, listNode1);
-		ListNode listNode2 = new ListNode(2, listNode3);
-		listNode1 = new ListNode(1, listNode2);
-		
-		ListNode listNode6 = new ListNode(4);
-		ListNode listNode5 = new ListNode(3, listNode6);
-		ListNode listNode4 = new ListNode(1, listNode5);
+		ListNode listNode2 = null;		
+		ListNode listNode4 = new ListNode(-4, listNode2);
+		ListNode listNode3 = new ListNode(0, listNode4);
+		listNode2 = new ListNode(2, listNode3);
+		ListNode listNode1 = new ListNode(3, listNode2);
 		
 		System.out.print(linkedListCycle.hasCycle(listNode1));
 	}
